5b17dfd9c395a3f0f285992f35bcb8152ecdc7d0
GabrieleMajocchi <gabriele.majocchi@gmail.com>
2023-08-22 20:16:13 +0200

    added project controller to bring users to different admin pages and to show him proper data

---
+++ app/Http/Controllers/ProjectController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Project;
+
+class ProjectController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $projects = Project::all();
+        return view('admin.projects.index', compact('projects'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('admin.projects.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title'=> 'required|unique:projects|min:3|max:255',
+            'description'=> 'required|min:3|max:255',
+            'lang'=> 'required|min:3|max:255',
+            'link'=> 'required|unique:projects|min:5|max:255'
+        ]);
+        $data = $request->all();
+
+        $newProject = new Project();
+        $newProject->title = $data['title'];
+        $newProject->description = $data['description'];
+        $newProject->lang = $data['lang'];
+        $newProject->link = $data['link'];
+        $newProject->date = date('y-m-d');
+        $newProject->save();
+        return redirect()->route('projects.show', $newProject->id);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $project = Project::findOrFail($id);
+        return view('admin.projects.show', compact('project'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
