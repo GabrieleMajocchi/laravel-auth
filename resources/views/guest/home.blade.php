2fbdd98ba9e89605438f2bdc20e82ba7a0075eb7
GabrieleMajocchi <gabriele.majocchi@gmail.com>
2023-08-22 20:14:31 +0200

    created home for guest

---
+++ resources/views/guest/home.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-12 d-flex">
+            <h1 class="m-auto">
+                Welcome to Gabriele Majocchi Portfolio site!
+            </h1>
+        </div>
+    </div>
+</div>
+@endsection
