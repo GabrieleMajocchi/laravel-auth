5598faf7a4e9b63a0672dda9a5608850edc5336c
GabrieleMajocchi <gabriele.majocchi@gmail.com>
2023-08-22 20:13:42 +0200

    added button to admin index

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,6 +15,8 @@
                     @endif
 
                     {{ __('You are logged in!') }}
+
+                    <a href="{{ route('admin.index') }}" class="btn btn-primary d-block">Admin HomePage</a>
                 </div>
             </div>
         </div>
