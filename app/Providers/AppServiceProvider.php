06b7ed501d0e44d1ab00fa7af10e2f066befdc87
GabrieleMajocchi <gabriele.majocchi@gmail.com>
2023-08-23 17:36:33 +0200

    fixed tailwind for pagination

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
+use Illuminate\Pagination\Paginator;                                        #AGGIUNGIAMO IL PAGINATOR
+
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -19,6 +21,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Paginator::useBootstrap();                                        #AGGIUNGIAMO IL PAGINATOR E USIAMO BOOTSTRAP
     }
 }
