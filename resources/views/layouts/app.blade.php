bf6fa80f42b2e335bd578f0d0eb66daa4786ff8b
GabrieleMajocchi <gabriele.majocchi@gmail.com>
2023-08-23 18:51:34 +0200

    changed navbar home button to bring user to proper seciton based on auth

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,9 +20,15 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
+                @auth
+                <a class="navbar-brand" href="{{ url('/admin/home') }}">
+                    {{ config('app.name', 'Laravel') }}
+                </a>
+                @else
                 <a class="navbar-brand" href="{{ url('/') }}">
                     {{ config('app.name', 'Laravel') }}
                 </a>
+                @endauth
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
