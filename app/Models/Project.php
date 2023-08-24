be417e2f2bf49874c3cfc5e88704c63c44e53f3b
GabrieleMajocchi <gabriele.majocchi@gmail.com>
2023-08-24 16:19:02 +0200

    added image as fillable

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -12,6 +12,6 @@ class Project extends Model
     use SoftDeletes;
 
     protected $fillable = [
-        'title', 'description', 'lang', 'link', 'date',
+        'title', 'description', 'lang', 'link', 'date', 'image'
     ];
 }
